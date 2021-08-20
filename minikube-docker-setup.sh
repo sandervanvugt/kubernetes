4b41a90e65177bf3dcdcd42cd03990127c6ea885
sandervanvugt <mail@sandervanvugt.nl>
2021-08-20 11:48:41 +0200

    updated install scripts

---
+++ minikube-docker-setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# added 20 Aug. 2021
+# currently only supported on Ubuntu 20.04 LTS
+
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+####
+echo before using minikube start --vm-driver=docker
+echo manually use usermod-aG docker your-user-name
+echo next type newgrp docker or use reboot to restart
+echo press enter to continue
+read
+
+minikube start --vm-driver=docker
