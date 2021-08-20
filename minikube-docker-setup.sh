da45871bd8d37e7ce6c376fe251420d512f71e38
sandervanvugt <mail@sandervanvugt.nl>
2021-08-20 14:07:03 +0200

    final tuning minikube-docker-setup.sh

--- minikube-docker-setup.sh
+++ minikube-docker-setup.sh
@@ -16,6 +16,9 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 ####
+echo the script is now ready
+echo manually run minikube start --vm-driver=docker to start minikube
+
 sudo usermod -aG docker $USER
 newgrp docker
 
